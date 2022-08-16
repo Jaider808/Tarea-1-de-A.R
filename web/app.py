ed46df75ff0de4e95e5a8e0158e15786e375f98a
Jaider808 <babiloniaj@utb.edu.co>
2022-08-15 20:17:43 -0500

    se iso la tarea

--- web/app.py
+++ web/app.py
@@ -33,6 +33,37 @@ def people():
     print(data)
     return render_template('people.html', value=data)
 
+@app.route('/person_delete/<string:id_person>')
+def people_delete(id_person):
+    for person_delete in model:
+        if person_delete.id_person==id_person:
+            model.remove(person_delete)
+    return render_template('person_detail.html', value="Was delete")
+
+
+@app.route('/person_update/<string:id_person>')
+def people_update(id_person):
+    persona = None
+    for person_update in model:
+        if person_update.id_person==id_person:
+            persona = person_update
+            break
+    return render_template('person_update.html',value=persona)
+
+
+@app.route('/person_updated', methods=['POST'])
+def people_updated():
+    id_person = request.form['id_person']
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    for person_updated in model:
+        if person_updated.id_person==id_person:
+            person_updated.name = first_name
+            person_updated.last_name = last_name
+            break
+    return render_template('person_detail.html', value = "Was updated")
+
+
 
 if __name__ == '__main__':
     app.run()
